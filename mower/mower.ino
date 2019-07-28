d0c1f40a127f357cba5d093926012637c76a87e4
Dennis Henke <dennis.henke@outlook.com>
2019-07-28 14:21:35 +0200

    Changed Pin, removed Delay

--- mower/mower.ino
+++ mower/mower.ino
@@ -9,9 +9,10 @@
 #include <WiFiManager.h>  //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 
 //Global variables
-#define GoPin 3
-#define StopPin 4
+#define StopPin D5
+#define GoPin D6
 #define WEBSRVPORT 80
+#define PressDuration 600
 bool running = false;
 //Fallback WiFi AP credentials
 char wifiap[] = "Meow";
@@ -24,13 +25,28 @@ ESP8266WebServer server(WEBSRVPORT);
 void pressButton(bool go) {
   if (go) {
     Serial.println("Go go go!");
+    digitalWrite(LED_BUILTIN, LOW);
     digitalWrite(GoPin, HIGH);
-    delay(600);
+    unsigned long previousMillis = millis();
+    while(true) {
+      if (millis() - previousMillis >= PressDuration) {
+        break;
+      }
+    }
+    digitalWrite(LED_BUILTIN, HIGH);
     digitalWrite(GoPin, LOW);
   } else {
     Serial.println("Stopping. Narf.");
+    digitalWrite(LED_BUILTIN, LOW);
     digitalWrite(StopPin, HIGH);
-    delay(600);
+    unsigned long previousMillis = millis();
+    while(true) {
+      if (millis() - previousMillis >= PressDuration) {
+        break;
+      }
+    }
+    delay(PressDuration);
+    digitalWrite(LED_BUILTIN, HIGH);
     digitalWrite(StopPin, LOW);
   }
 }
@@ -64,12 +80,15 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(115200);
+  Serial.println("Booted. Please wait.");
   pinMode(GoPin, OUTPUT);
   pinMode(StopPin, OUTPUT);
   digitalWrite(GoPin, LOW);
   digitalWrite(StopPin, LOW);
   WiFiManager wifiManager;
   //wifiManager.resetSettings(); //reset WiFi settings
+  Serial.print("Trying to connect to ");
+  Serial.println(wifiap);
   wifiManager.autoConnect(wifiap, wifipw);
   Serial.println("Connected...starting Webserver");
   digitalWrite(LED_BUILTIN, HIGH);
